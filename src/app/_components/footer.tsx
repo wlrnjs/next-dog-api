c914de3db4372c3f51280d4f73b96c5ad7e21405
wlrnjs <tjwlrnjs7336@icloud.com>
2024-12-22 16:23:31 +0900

    디자인 완성

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,5 +1,9 @@
+import style from "./footer.module.css"
+
 export default function Footer() {
   return (
-    <div>jiKwon</div>
+    <div className={style.p}>
+      <p>jiKwon</p>
+    </div>
   )
 }
